4774350d94b665c53d54bb2365fd5119ad7bdb31
Vladislav <78873753+boyar-vd@users.noreply.github.com>
2023-07-26 18:01:19 +0300

    materials for lesson 3

--- Lesson 2/HSE_DPO_DWH_Airflow/dags/dag_raw_to_dds.py
+++ Lesson 2/HSE_DPO_DWH_Airflow/dags/dag_raw_to_dds.py
@@ -10,6 +10,10 @@ from dags.python_scripts import update_table_merchants
 '''Import args for DAG'''
 from python_scripts.config import dag_args
 
+
+def foo():
+    pass
+
 with DAG('dag_name',
          description='dag_tag',
          schedule_interval='* * * * *',  # every minute
@@ -17,6 +21,10 @@ with DAG('dag_name',
          default_args=dag_args
          ) as dag:
     update_table_merchants_task = PythonOperator(task_id='update_table_merchants',
-                                                 python_callable=update_table_merchants.update())
+                                                 python_callable=update_table_merchants.update)
+    update_table_customers_task = PythonOperator(task_id='update_table_customers',
+                                                 python_callable=update_table_customers.update)
+    update_table_transactions_task = PythonOperator(task_id='update_table_transactions',
+                                                 python_callable=update_table_transactions.update)
     '''DAG pipeline'''
-    update_table_merchants_task
+    task_zero >> [update_table_merchants_task, update_table_customers_task] >> update_table_transactions_task
--- Lesson 2/HSE_DPO_DWH_Airflow/dags/python_scripts/update_table_merchants.py
+++ Lesson 2/HSE_DPO_DWH_Airflow/dags/python_scripts/update_table_merchants.py
@@ -3,7 +3,7 @@ Update table "DDS".merchants
 """
 
 '''Import libs'''
-# from sqlalchemy import text
+from sqlalchemy import text
 import pandas as pd
 
 '''Import connections and common functions'''
@@ -12,18 +12,23 @@ from dags.python_scripts.config import engine
 
 def update():
     sql = f'''
-    SELECT DISTINCT merchant
-     FROM "RAW".test_table_sl
+    SELECT DISTINCT merchant as "name"
+      FROM "RAW".test_table_sl
     '''
 
     select_df = pd.read_sql(sql=sql, con=engine)
 
     if not select_df.empty:
         # select_df.apply(lambda row: function(row), axis=1)
-
+        # engine.execute(text('''
+        # INSERT INTO "DDS".merchants_test
+        # ("name")
+        # VALUES
+        # ()
+        # '''))
         select_df.to_sql(name='merchants_test'
                         ,con=engine
                         ,schema='DDS'
                         ,if_exists='replace'
-                        ,index=True)
+                        ,index=False)
 # update()
